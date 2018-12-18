c24ade3dfd53cfb6dd6b47a12192ada6b29491e6
Phoebe Nyawira <ph.gathuku@gmail.com>
2018-12-18 18:48:21 +0300

    Added blockquotes to bikeshare

--- bikeshare.py
+++ bikeshare.py
@@ -44,6 +44,7 @@ def get_filters(): load_data(city, month, day)
 All = 'all'
 print('Hello! Let\'s explore some US bikeshare data!')
 
+Quote break
     # TO DO: get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
 
 while True:
